ff823fbf95e0ff6bb61793bb5c821a224c71a54a
Park Sung Min <vamf12@gmail.com>
2021-06-14 20:49:36 +0900

    Revert "log turn off by default"
    This reverts commit 75ba8c93

--- kenet-server-engine-http/build.gradle.kts
+++ kenet-server-engine-http/build.gradle.kts
@@ -18,6 +18,13 @@ kotlin {
         }
 
         @Suppress("UNUSED_VARIABLE")
+        val jvmMain by getting {
+            dependencies {
+                implementation("org.slf4j:slf4j-simple:1.7.25")
+            }
+        }
+
+        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
