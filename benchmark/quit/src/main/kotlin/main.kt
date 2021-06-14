501ee2b0b0faf7ce0d81710d57a87fb61a49ab41
Park Sung Min <vamf12@gmail.com>
2021-06-14 20:04:51 +0900

    실행대기 200ms

--- benchmark/quit/src/main/kotlin/main.kt
+++ benchmark/quit/src/main/kotlin/main.kt
@@ -4,7 +4,7 @@ import org.kotlin.everywhere.net.createClient
 import org.kotlin.everywhere.net.invoke
 
 fun main() = runBlocking {
-    delay(150) // 서버 실행까지 대기
+    delay(200) // 서버 실행까지 대기
 
     val client = createClient(Api())
     client.kenet.benchmark(Unit)
