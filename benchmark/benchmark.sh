ec13f3b729c9887ed436502245be67c5b607e50f
Park Sung Min <vamf12@gmail.com>
2021-06-14 17:24:52 +0900

    client sleep 타이밍 조절

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -13,15 +13,17 @@ trap 'sleep 1 && (jobs -p | xargs -r kill -9)' EXIT
 
 export TZ=Asia/Seoul
 
+rm -f result-*.txt
+
 date >>result.txt
 echo "" >>result.txt
 
 ./benchmark-2-memory-usage/build/install/benchmark-2-memory-usage/bin/benchmark-2-memory-usage &
-sleep 1
+sleep 0.1
 ./quit/build/install/quit/bin/quit
 sleep 1
 
 java -jar benchmark-3-execution-image-size/build/libs/benchmark-3-execution-image-size-all.jar &
-sleep 1
+sleep 0.1
 ./quit/build/install/quit/bin/quit
 sleep 1
