47e3e447a2319c132d88000724e246a0f3ea44a2
Park Sung Min <vamf12@gmail.com>
2021-06-14 20:52:00 +0900

    turn on log

--- kenet-client/build.gradle.kts
+++ kenet-client/build.gradle.kts
@@ -20,6 +20,13 @@ kotlin {
         }
 
         @Suppress("UNUSED_VARIABLE")
+        val jvmMain by getting {
+            dependencies {
+                implementation("org.slf4j:slf4j-simple:1.7.25")
+            }
+        }
+
+        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
