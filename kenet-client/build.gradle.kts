8d0f58f661e254a7c2fe42c774bbd5082a8848a0
Park Sung Min <vamf12@gmail.com>
2021-06-14 21:17:22 +0900

    Revert "turn on log"
    This reverts commit 47e3e447

--- kenet-client/build.gradle.kts
+++ kenet-client/build.gradle.kts
@@ -20,13 +20,6 @@ kotlin {
         }
 
         @Suppress("UNUSED_VARIABLE")
-        val jvmMain by getting {
-            dependencies {
-                implementation("org.slf4j:slf4j-simple:1.7.25")
-            }
-        }
-
-        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
