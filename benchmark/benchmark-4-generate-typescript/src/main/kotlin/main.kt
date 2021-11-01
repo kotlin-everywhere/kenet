06550b492a2c5e7676311ccfd778ff4d59c31f64
Sung Min Park <vamf12@gmail.com>
2021-11-01 16:49:17 +0900

    docs(benchmark): 생성 소요 시간 결과에 대한 주석 추가

--- benchmark/benchmark-4-generate-typescript/src/main/kotlin/main.kt
+++ benchmark/benchmark-4-generate-typescript/src/main/kotlin/main.kt
@@ -6,17 +6,20 @@ import org.kotlin.everywhere.net.gen.typescript.generate
 import kotlin.io.path.Path
 
 fun main() {
+    // 프로그램 시작 시각
     val initialTimes = System.currentTimeMillis()
 
     val kenet = Api()
+    // 정의된 10000개 이상의 데이터 dist 아래에 api.ts 로 생성
     generate(kenet, Path("benchmark-4-generate-typescript/dist"), "api", Deno)
 
+    // 생성에 소요된 시간 = 현재시각 - 프로그램 시작 시각
     val generateTimes = System.currentTimeMillis() - initialTimes
 
     log(
         "4. generate-typescript",
-        "endpoint-counts: ${sumEndpointSize(kenet)}",
-        "generate-times: ${generateTimes}ms",
+        "endpoint-counts: ${sumEndpointSize(kenet)}" /* 데이터 갯수 */,
+        "generate-times: ${generateTimes}ms" /* 생성에 소요된 시간 */,
     )
 
     logResult("4-generate-typescript", generateTimes)
