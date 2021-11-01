16dd1f41e422d57c7d7903b94112f32989f2099f
Sung Min Park <vamf12@gmail.com>
2021-11-01 16:40:15 +0900

    docs(benchmark): 메모리 사용량 관련 주석 추가

--- benchmark/benchmark-2-memory-usage/src/main/kotlin/main.kt
+++ benchmark/benchmark-2-memory-usage/src/main/kotlin/main.kt
@@ -5,13 +5,14 @@ import org.kotlin.everywhere.net.invoke
 
 fun Api.init(mainScope: CoroutineScope, quitDeferred: CompletableDeferred<Unit>) {
     benchmark {
+        // 사용중인 메모리 =  JVM 에 설정된 최대 메모리 - JVM 에 남은 메모리
         val usedMemory = Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory()
         log(
             "2. memory-usage",
-            "total memory: ${Runtime.getRuntime().totalMemory().toMb()}",
-            "max memory: ${Runtime.getRuntime().maxMemory().toMb()}",
-            "free memory: ${Runtime.getRuntime().freeMemory().toMb()}",
-            "used memory: ${usedMemory.toMb()}",
+            "total memory: ${Runtime.getRuntime().totalMemory().toMb()}" /* JVM 전체 메모리 */,
+            "max memory: ${Runtime.getRuntime().maxMemory().toMb()}" /* JVM 최대 메모리 */,
+            "free memory: ${Runtime.getRuntime().freeMemory().toMb()}" /* JVM 미사용 메모리 */,
+            "used memory: ${usedMemory.toMb()}" /* 사용중인 메모리 */,
         )
         logResult("2-memory-usage", usedMemory)
     }
